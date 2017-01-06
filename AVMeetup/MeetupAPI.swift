9c86aa5277617c503e0c40711a5a9ab131da70cb
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-06 18:06:21 -0500

    Setup basic folder structure

---
+++ AVMeetup/MeetupAPI.swift
@@ -0,0 +1,9 @@
+//
+//  MeetupAPI.swift
+//  AVMeetup
+//
+//  Created by Ayuna NYC on 1/6/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
---
+++ AVMeetup/MeetupRouter.swift
@@ -0,0 +1,9 @@
+//
+//  MeetupRouter.swift
+//  AVMeetup
+//
+//  Created by Ayuna NYC on 1/6/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
