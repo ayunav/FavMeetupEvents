9f5b36e478a70e9513e2d8f940921f21a8a59bac
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-06 18:12:54 -0500

    Add a custom table view cell

---
+++ AVMeetup/EventsTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  EventsTableViewCell.swift
+//  AVMeetup
+//
+//  Created by Ayuna NYC on 1/6/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+    
+    /*
+     The event name
+     The Meetup Group name
+     The event date and time
+     How many people have RSVP’d yes so far
+     */
+    
+    // eventNameLabel
+    // groupNameLabel
+    // eventDateAndTimeLabel
+    // rsvpCountLabel 
+    // favoriteStateButton
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
