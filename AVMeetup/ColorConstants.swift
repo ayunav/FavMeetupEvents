c4c3c02ca4a69e23066b5881ed76c5f344f63bff
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-08 13:00:57 -0500

    Add ColorConstants file for MeetupRedColor constant

---
+++ AVMeetup/ColorConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ColorConstants.swift
+//  AVMeetup
+//
+//  Created by Ayuna NYC on 1/8/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import UIKit
+
+// HEX# ed1c40 - http://eye-dropper.kepi.cz/ tool
+// Get RGB from HEX - http://uicolor.xyz/ - amazing
+
+let MeetupRedColor = UIColor(red:0.93, green:0.11, blue:0.25, alpha:1.0)
