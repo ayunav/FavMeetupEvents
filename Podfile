cb2cd5a2aa1225ec6cdc03209f41014b88dc128b
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-08 21:38:36 -0500

    Add Alamofire CocoaPod

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'AVMeetup' do
+    use_frameworks!
+    pod 'Alamofire', '~> 4.2'
+end
---
+++ Pods/Target Support Files/Pods-AVMeetup/Pods-AVMeetup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AVMeetup : NSObject
+@end
+@implementation PodsDummy_Pods_AVMeetup
+@end
