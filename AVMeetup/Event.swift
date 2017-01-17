2a801b614303ad7c421748be8818fdf8e80d1c44
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-16 23:13:01 -0500

    Add NSCoding protocol to Event model

--- AVMeetup/Event.swift
+++ AVMeetup/Event.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-enum EventFields: String
-{
+enum EventFields: String {
+
     case eventId      = "id"
     case group        = "group"
     case groupUrlName = "urlname"
@@ -20,8 +20,8 @@ enum EventFields: String
 }
 
 
-class Event
-{
+class Event: NSObject, NSCoding {
+    
     let date     : String
     let eventId  : String
     let group    : String
@@ -30,86 +30,45 @@ class Event
     let time     : String
     let venue    : String
     
-    
-    //var isLiked  : Bool?
-
     var isLiked  : Bool = false {
         didSet {
             
-//            EventsStore.sharedInstance.store(favState: isLiked)
-            
-            
-            // get userDefaults favEventsIDs
-            // check is eventId is in the arr 
-            // if not, add to the arr, 
-            // if it is, remove from the arr
-            // update favEventsArr
-            // synchronize
-            
-            var favoritedEvents = UserDefaults.standard.array(forKey: "favoritedEvents")! // guard?
-            
             if (isLiked) {
-                // if event was liked aka heart button was pressed, 
-                // check is the eventId is in the fav events IDs array
-                
-                let wasFavoritedBool = favoritedEvents.contains { $0 as! String == eventId }
-                
-                // if the eventId is not in the fav Events IDs array, add eventId to the arr
-                if wasFavoritedBool == false {
-                    favoritedEvents.append(eventId)
+                if (EventsStore.sharedInstance.contains(for: self.eventId) == true) {
+                    EventsStore.sharedInstance.remove(for: self.eventId)
+                } else {
+                    EventsStore.sharedInstance.set(value: self, for: self.eventId)
                 }
-                // if the event is already in the array of fav events IDs, 
-                // remove the eventID from the array
-//                else {
-//                    let updatedFavEvents = favoritedEvents.filter( { $0 as! String != eventId })
-//                    favoritedEvents = updatedFavEvents
-//                    UserDefaults.standard.set(favoritedEvents, forKey:"favoritedEvents")
-//                }
-                UserDefaults.standard.set(favoritedEvents, forKey:"favoritedEvents")
+            } else {
+                EventsStore.sharedInstance.remove(for: self.eventId)
             }
-            // if the event was unliked
-            else {
-                // remove eventId from the fav events IDs array
-                let updatedFavEvents = favoritedEvents.filter( { $0 as! String != eventId })
-                UserDefaults.standard.set(updatedFavEvents, forKey:"favoritedEvents")
-            }
-            UserDefaults.standard.synchronize()
         }
     }
 
     
-    init(date: String, eventId: String, group: String, name: String, rsvp: String, time: String, venue: String)
-    {
-        self.date      = date
-        self.eventId   = eventId
-        self.group     = group
-        self.name      = name
-        self.rsvp      = rsvp
-        self.time      = time
-        self.venue     = venue
-
-        // if we have favEventsArr 
-        // and if the eventId is in the arr, set isLiked property to true 
-        // if no favEventsArr doesn't exist 
-        // set new arr for key favEvents
-        // set isLiked property to false
+    
+    init(date: String, eventId: String, group: String, name: String, rsvp: String, time: String, venue: String) {
         
-        //self.isLiked = EventsStore.sharedInstance.isLiked()
+        self.date    = date
+        self.eventId = eventId
+        self.group   = group
+        self.name    = name
+        self.rsvp    = rsvp
+        self.time    = time
+        self.venue   = venue
         
-        if UserDefaults.standard.object(forKey: "favoritedEvents") != nil {
-            self.isLiked = UserDefaults.standard.array(forKey: "favoritedEvents")!.contains { $0 as! String == eventId }
-        } else {
-            UserDefaults.standard.set(NSArray(), forKey:"favoritedEvents")
-            self.isLiked = false
-        }
+        self.isLiked = EventsStore.sharedInstance.contains(for: self.eventId)
     }
+    
+
+    // MARK: - Public API
     
     
     private static let dateFormatter = DateFormatter()
     
     
-    static func eventFromJsonDict(json: [String: AnyObject]) -> Event?
-    {
+    static func eventFromJsonDict(json: [String: AnyObject]) -> Event? {
+        
         guard let time    = json[EventFields.time.rawValue]      as? Int,
             let rsvpCount = json[EventFields.rsvpCount.rawValue] as? Int,
             let name      = json[EventFields.name.rawValue]      as? String,
@@ -124,4 +83,48 @@ class Event
         
         return Event(date: eventDate, eventId: eventId, group: groupName, name: name, rsvp: rsvp, time: eventTime, venue: venue)
     }
+    
+    
+    // MARK: - NSCoding
+    
+
+    required convenience init?(coder aDecoder: NSCoder) {
+
+        guard let date  = aDecoder.decodeObject(forKey: "date")    as? String,
+            let name    = aDecoder.decodeObject(forKey: "name")    as? String,
+            let rsvp    = aDecoder.decodeObject(forKey: "rsvp")    as? String,
+            let time    = aDecoder.decodeObject(forKey: "time")    as? String,
+            let venue   = aDecoder.decodeObject(forKey: "venue")   as? String,
+            let group   = aDecoder.decodeObject(forKey: "group")   as? String,
+            let eventId = aDecoder.decodeObject(forKey: "eventId") as? String
+            else { return nil }
+
+        
+        self.init(
+            date   : date,
+            name   : name,
+            rsvp   : rsvp,
+            time   : time,
+            venue  : venue,
+            group  : group,
+            eventId: eventId
+        )
+        
+        self.isLiked = aDecoder.decodeBool(forKey: "isLiked")
+    }
+    
+    
+    func encode(with aCoder: NSCoder) {
+        
+        aCoder.encode(self.date,    forKey: "date")
+        aCoder.encode(self.name,    forKey: "name")
+        aCoder.encode(self.rsvp,    forKey: "rsvp")
+        aCoder.encode(self.time,    forKey: "time")
+        aCoder.encode(self.group,   forKey: "group")
+        aCoder.encode(self.venue,   forKey: "venue")
+        aCoder.encode(self.eventId, forKey: "eventId")
+        aCoder.encode(self.isLiked, forKey: "isLiked")
+    }
+
+    
 }
