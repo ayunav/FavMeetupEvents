79cb672a6234aa88782c41168ade2cfc80de13d6
Ayuna NYC <ayunav@users.noreply.github.com>
2017-01-17 10:51:00 -0500

    Add Constants file with the api key

---
+++ AVMeetup/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  AVMeetup
+//
+//  Created by Ayuna NYC on 1/6/17.
+//  Copyright © 2017 Ayuna NYC. All rights reserved.
+//
+
+import Foundation
+
+let MeetupApiKey = "6752511f3291b2b182ee4d2ef312"
